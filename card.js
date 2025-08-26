bc18a511872b799b1d51bb309f6dbc9eed123337
Solanki Divya <divusolanki07@gmail.com>
2025-08-26 17:23:30 +0530

    Update card.js

--- card.js
+++ card.js
@@ -1,40 +1,3 @@
-// var propfileFrame = document.querySelector('.img-frame');
-// var userName = "Solanki Divya";
-// var userProfession = "Web Developer";
-// var userEmail =  "divusolanki07@gmail.com";
-// var userPhone = "+91 75674 40153";
-
-// var profileImage = "images/profile.jpg";
-// var name = "Solanki Divya";
-// var profession = "Web Developer";
-// var email = "divusolanki07@gmail.com";
-// var phone = "+91 75674 40153";
-
-// profileFrame.innerHTML = '<img src="' + profileImage + '" alt="Profile Image">';
-// userName.textContent = name;
-// propfileFrame.innerHTML = '<img src="./images/profile.jpg"' + profileImage + '" alt="Profile Image"> style="width: 50px; height: 90px;">';
-
-// profileFrame.innerHTML = '<img src="' + profileImage + '" alt="Profile Image" style="width:50px; height:90px;">';
-
-
-// var profileFrame = document.querySelector('.img-frame');
-
-// var userName = "Solanki Divya";
-// var userProfession = "Web Developer";
-// var userEmail =  "divusolanki07@gmail.com";
-// var userPhone = "+91 75674 40153";
-
-// var profileImage = "./images/profile.jpg";
-// var name = "Solanki Divya";
-// var profession = "Web Developer";
-// var email = "divusolanki07@gmail.com";
-// var phone = "+91 75674 40153";
-
-
-// profileFrame.innerHTML = '<img src="' + profileImage + '" alt="Profile Image" style="width:50px; height:90px;">';
-
-
-
 // Select elements
 var profileFrame = document.querySelector('.img-frame');
 var nameDiv = document.querySelector('.name');
@@ -57,3 +20,4 @@ nameDiv.textContent = userName;
 professionDiv.textContent = userProfession;
 emailDiv.textContent = userEmail;
 contactDiv.textContent = userPhone;
+
